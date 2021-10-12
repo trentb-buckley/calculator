37bc4d54ca3c8859dd21a50cd9de58ebe04cf4f6
Trenton Buckley <trentb.buckley@gmail.comtrentb.buckley@gmail.comqq>
2021-10-12 16:00:17 -0600

    Basic Fn Commit

--- calc.js
+++ calc.js
@@ -5,15 +5,32 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
+// const plus = "+";
 
 reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
-	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+    tokens = input.split(' ');
+    
+    mathSymbol = tokens[0];
+    num1 = Number(tokens[1]);
+    num2 = Number(tokens[2]);
 
+    console.log('mathSymbol', mathSymbol);
+    console.log('num1', num1);
+    console.log('num2', num2);
+
+    if(mathSymbol === "+") {
+        console.log(`Answer: ${num1 + num2}`)
+    } else if(mathSymbol === "-") {
+		console.log(`Answer: ${num1 - num2}`)
+	} else if(mathSymbol === "*") {
+		console.log(`Answer: ${num1 * num2}`)
+	} else if(mathSymbol === "/") {
+		console.log(`Answer: ${num1 / num2}`)
+	} else if(mathSymbol === "sqrt") {
+		console.log(`Answer: ${Math.sqrt(num1)}`)
+	} else if (mathSymbol === "sq") {
+		console.log(`Answer: ${num1 *num1}`)
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
